1169a1c91bbf1baf874f8c57850e75c7a78e9a6a
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2019-01-21 13:32:41 +0100

    pagina admin

---
+++ Web/wp-admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Jotun</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Neuton" rel="stylesheet">
+    <link rel="stylesheet" href="view/scss/style.css">
+    <script src="view/js/js.js"></script>
+
+</head>
+<body>
+
+<div class="container">
+
+    <form class="mt-5">
+        <div class="form-group">
+            <label for="nombreAdmin">Nombre</label>
+            <input type="text" class="form-control" id="nombreAdmin" placeholder="Nombre">
+        </div>
+        <div class="form-group">
+            <label for="passwordAdmin">Password</label>
+            <input type="password" class="form-control" id="passwordAdmin" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+</div>
+
+</body>
+</html>
