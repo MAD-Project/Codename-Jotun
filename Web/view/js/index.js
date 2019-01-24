702a8629877ae4199fc3e5e6bb96b4f2ae4d268d
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2019-01-24 10:39:14 +0100

    Estructura arreglada

--- Web/view/js/index.js
+++ Web/view/js/index.js
@@ -1,38 +1,42 @@
 // This function will be executed when the user scrolls the page.
-$(window).scroll(function(e) {
+$(window).scroll(function (e) {
     // Get the position of the location where the scroller starts.
     var scroller_anchor = $(".scroller_anchor").offset().top;
-    
-    // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top 
-    if ($(this).scrollTop() >= scroller_anchor && $('#scroller').css('position') != 'fixed') 
-    {    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+
+    // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top      && localStorage.getItem("carritoActivado") != null 
+    if ($(this).scrollTop() >= scroller_anchor && $('#scroller').css('position') != 'fixed' ) { //&& localStorage.getItem("carritoActivado") != null
         $('#scroller').removeClass("d-none");
         $('#scroller').addClass("fixed-top carrito-position");
-    } 
-    else if ($(this).scrollTop() < scroller_anchor && $('#scroller').css('position') != 'relative') 
-    {    // If the user has scrolled back to the location above the scroller anchor place it back into the content.
-        
-        // Change the height of the scroller anchor to 0 and now we will be adding the scroller back to the content.
-        
-        // Cambio a su posicion original.
-        $('#scroller').addClass("d-none");
-        $('#scroller').removeClass("fixed-top carrito-position");
     }
-
 });
 
-function mostrarBtn() {
-    $('#carritobtn').removeClass('hidden');
+$(window).scroll(function (e) {
+    // Get the position of the location where the scroller starts.
+    var scroller_anchor = $("#nav").offset().top;
+
+    // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top      && localStorage.getItem("carritoActivado") != null 
+    if ($(this).scrollTop() >= scroller_anchor && localStorage.getItem("scrollDone") != 1) { // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+        $('html, body').animate({
+            scrollTop: $("#productosDiv").offset().top
+        }, 1000);
+        localStorage.setItem("scrollDone", 1);
+    }
+});
+
+function irCarrito() {
+    $('html, body').animate({
+        scrollTop: $("#carrito").offset().top
+    }, 1000);
 }
 
 function rotateArrow(id) {
     if ($('#' + id).hasClass("rotate")) {
-        $('#' + id).removeClass("rotate"); 
-        $('#' + id).addClass("rotate-origin"); 
-        setTimeout(function() {
+        $('#' + id).removeClass("rotate");
+        $('#' + id).addClass("rotate-origin");
+        setTimeout(function () {
             $('#' + id).removeClass("rotate-origin");
         }, 500);
     } else {
-        $('#' + id).addClass("rotate"); 
+        $('#' + id).addClass("rotate");
     }
 }
