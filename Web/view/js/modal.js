6f78b6e66b66e8a81a7ec26fc595c3bd10f5204f
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2019-01-24 10:49:57 +0100

    1 modal para todos los productos y empezando con carrito

---
+++ Web/view/js/modal.js
@@ -0,0 +1,34 @@
+$('#modalProductos').on('show.bs.modal', function (event) {
+
+    var button = $(event.relatedTarget);
+    var precio = button.data('precio');
+    var nombre = button.data('nombre');
+    var medida = button.data('medida');
+    var id = button.data('id');
+    var unidad = button.data('cantidad');
+
+    $('#unidades').prop("min",unidad);
+
+    switch (medida){
+
+        case "R":
+            medida = "Ración";
+            break;
+
+        case "K":
+            medida = "Kilo";
+            break;
+
+        case "U":
+            medida = "Unidad";
+            break;
+    }
+
+    var modal = $(this);
+    modal.find('.modal-body .nProducto').html("Nombre : "+nombre);
+    modal.find('.modal-body .pProducto').html("Precio: "+precio+"€ / "+medida);
+
+    modal.find('.modal-body .nombreProducto').val(nombre);
+    modal.find('.modal-body .precioProducto').val(precio);
+    modal.find('.modal-body .idProducto').val(id);
+});
