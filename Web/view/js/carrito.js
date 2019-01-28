5462f098e725b6b12f4552b130e05c2530c5020c
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2019-01-28 10:44:49 +0100

    c

--- Web/view/js/carrito.js
+++ Web/view/js/carrito.js
@@ -1,19 +1,7 @@
-
 function carrito() {
 
     $('#scroller').fadeIn("slow");
     $('#scroller').addClass("fixed-top carrito-position");
-
-    var unidades = $('#unidadesProducto').val();
-    var numeroProductosCarrito = $('#numeroProductosCarrito').text();
-
-    var totalProductos = parseInt(unidades)+parseInt(numeroProductosCarrito);
-
-    $('#modalProductos').modal('hide');
-
-    $('#numeroProductosCarrito').html(totalProductos);
-
-    var productos = [];
 
     return false;
 }
