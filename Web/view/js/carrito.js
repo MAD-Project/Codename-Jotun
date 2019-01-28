6487bf5af0dc3fb1dc0613e7671550c404f8ce4c
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2019-01-28 10:46:35 +0100

    modal hiden

--- Web/view/js/carrito.js
+++ Web/view/js/carrito.js
@@ -3,5 +3,7 @@ function carrito() {
     $('#scroller').fadeIn("slow");
     $('#scroller').addClass("fixed-top carrito-position");
 
+    $('#modalProductos').modal('hide');
+
     return false;
 }
