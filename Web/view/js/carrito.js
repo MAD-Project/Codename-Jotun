39eadebc741df446fcbfca26780e5cdf3045b964
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2019-01-28 14:09:22 +0100

    Cambios

--- Web/view/js/carrito.js
+++ Web/view/js/carrito.js
@@ -5,3 +5,11 @@ function carrito() {
 
     return false;
 }
+
+function annadirCarrito(form) {
+    $("#carritoLista").append("<li class='p-4 list-group-item item-separation' style='border: none !important;'>" + form.elements[2].value + "<span>" + form.elements[3].value + " Unidades</span></li>");
+
+    $('#modalProductos').modal("hide");
+
+    return false;
+}
